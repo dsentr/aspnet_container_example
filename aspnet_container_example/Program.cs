c7937286254842f83faf2a69e72a76c5a45caee2
danielsenter <daniel.senter@bluekc.com>
2023-03-29 20:24:18 -0500

    init commit

---
+++ aspnet_container_example/Program.cs
@@ -0,0 +1,22 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+var app = builder.Build();
+
+app.MapHealthChecks("health");
+app.MapGet("/", () =>
+{
+    var config = app.Services.GetService<IConfiguration>();
+    var secretValue = config["danstestsecret1"];
+    if (secretValue != null)
+    {
+        return Results.Ok($"danstestsecret1 variable value is {secretValue}");
+    }
+    else
+    {
+        return Results.Ok("enviornment variable notfound");
+    }
+});
+
+app.Run();
+
